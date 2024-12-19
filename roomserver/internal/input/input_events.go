27a867595d878ef74d634958a2ab60a6fa6f86ec
Neil Alexander <neilalexander@users.noreply.github.com>
2024-12-19 18:05:11 +0000

    Revert "Add pprof annotations to roomserver worker goroutines"
    This reverts commit f0b9ac1dda61e42ca93ea44f891c1f367740e984.

--- roomserver/internal/input/input_events.go
+++ roomserver/internal/input/input_events.go
@@ -22,7 +22,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"runtime/pprof"
 	"time"
 
 	"github.com/neilalexander/harmony/roomserver/storage/tables"
@@ -116,10 +115,6 @@ func (r *Inputer) processRoomEvent(
 			"state_ids": len(input.StateEventIDs),
 		})
 	}
-
-	// Set goroutine labels, useful for pprof CPU profiles, useful for CPU
-	// pprof profiles to find out where time is being spent.
-	pprof.SetGoroutineLabels(context.WithValue(ctx, "room_id", event.RoomID().String()))
 
 	// Don't waste time processing the event if the room doesn't exist.
 	// A room entry locally will only be created in response to a create
